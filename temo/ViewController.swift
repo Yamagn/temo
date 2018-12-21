0156099b4ab949dc123832110e266392487a6b4b
Yamagn <syankenpon@gmail.com>
2018-12-21 17:54:30 +0900

    Initial Commit

---
+++ temo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  temo
+//
+//  Created by ymgn on 2018/12/21.
+//  Copyright © 2018 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
